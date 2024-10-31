998122c8e855734554602ddcf823294b6df411df
ChanRathke <111920689+ChanRathke@users.noreply.github.com>
2024-10-31 15:38:40 +0000

    fixed

--- testFolder/test.js
+++ testFolder/test.js
@@ -9,7 +9,7 @@ describe('Regular factorial value', function() {
 
 describe('First two terms', function() {
   it('should return 0 and 1 for first two terms', function() {
-      assert.strictEqual(main.factorial(0), 0);
+      assert.strictEqual(main.factorial(0), 1);
       assert.strictEqual(main.factorial(1), 1);
     });
 });
