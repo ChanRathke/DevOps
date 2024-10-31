a2423231f593e41d8322a71d96ee12f615a2cb96
ChanRathke <111920689+ChanRathke@users.noreply.github.com>
2024-10-31 15:51:21 +0000

    neg

--- testFolder/test.js
+++ testFolder/test.js
@@ -8,8 +8,14 @@ describe('Regular factorial value', function() {
 });
 
 describe('First two terms', function() {
-  it('should return 0 and 1 for first two terms', function() {
+  it('should return 1 for first two terms', function() {
       assert.strictEqual(main.factorial(0), 1);
       assert.strictEqual(main.factorial(1), 1);
     });
 });
+
+describe('Negative should return undefined', function() {
+    it('should return undefined', function() {
+        assert.strictEqual(main.factorial(-1), 'undefined');
+      });
+  });
